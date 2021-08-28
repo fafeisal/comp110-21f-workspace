65bd403fdcba46a303286047e4d8f4c113b52be1
Fatima Feisal <ffeisal@unc.edu>
2021-08-28 16:35:37 -0400

    Finished Input Practice

---
+++ lessons/input.py
@@ -0,0 +1,7 @@
+"""demonstrates asking the user for input"""
+
+__author__ = "730474766"
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name)
