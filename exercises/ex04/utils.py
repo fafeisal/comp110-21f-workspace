4f13029a34b6294b2350c3a7c613993cb046dbf2
Fatima Feisal <ffeisal@unc.edu>
2021-09-24 20:34:42 -0400

    Ex04 attempt 1

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,48 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730474766"
 
 
-# TODO: Implement your functions here.
+def all(items: list[int], number: int) -> bool:
+    """Verifying iff all items in list are the chosen int."""
+    i: int = 0
+    store: int = 0
+    while i < len(items):
+        position: int = items[i]
+        if position == number:
+            store += 1
+        i += 1
+    if store == len(items):
+        return True
+    else:
+        return False
+
+
+def is_equal(first: list[int], second: list[int]) -> bool:
+    """Verifying iff two lists are identical in value and index."""
+    i: int = 0
+    position: int = 0
+    while i < len(first) and len(second):
+        if first[i] == second[i]:
+            position += 1
+        i += 1
+    if position == len(first) and len(second):
+        return True
+    else:
+        return False
+
+
+def max(items: list[int]) -> int:
+    """Function to return largest vlaue from a list."""
+    if len(items) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    position: int = 0
+    next_item: int = 1
+    while i < len(items):
+        if items[next_item] > items[position]:
+            items[position] = items[next_item]
+        if next_item < len(items) - 1:
+            next_item += 1
+        i += 1
+    return items[position]
