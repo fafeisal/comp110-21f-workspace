0130a46d34842159b6f2c6f616ba18cd7dd73745
Fatima Feisal <ffeisal@unc.edu>
2021-10-21 13:20:05 -0400

    ex06 first attempt

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,58 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730474766"
 
-# Define your functions below
+
+def invert(main: dict[str, str]) -> dict[str, str]:
+    """Function for inverting dictionary's keys and values."""
+    inverted_dictionary: dict[str, str] = {}
+    for key in main:
+        for second_key in inverted_dictionary:
+            if second_key == main[key]:
+                raise KeyError("There are repeated keys in inverted_dictionary.")
+        inverted_dictionary[main[key]] = key
+    return inverted_dictionary
+
+
+def favorite_color(census: dict[str, str]) -> str:
+    """Finds highest frequency color in a dict of fav colors."""
+    # go through every color and add tallies to associated
+    # color in a secondary dictionary
+    totals: dict[str, int] = {}
+    most: str = ""
+    for key in census:
+        if census[key] in totals:
+            totals[census[key]] += 1
+        else:
+            totals[census[key]] = 1
+    # store the highest tallied color in another variable
+    for color in totals:
+        if most == "":
+            most = color
+        else: 
+            if totals[color] > totals[most]:
+                most = color
+    return most
+
+
+def count(items: list[str]) -> dict[str, int]:
+    """Counting frequency of items in a list."""
+    frequencies: dict[str, int] = {}
+    i: int = 0
+    while i < len(items):
+        current: str = items[i]
+        # place: int = 0
+        # holder: int = 0
+        # while place < len(items):
+        #     if current == items[place]:
+        #         holder += 1
+        #         items.pop(place)
+        #     else:
+        #         place += 1
+        # frequencies[current] = holder
+        if current in frequencies: 
+            frequencies[current] += 1
+        else:
+            frequencies[current] = 1
+        i += 1
+    return frequencies
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
+import pytest
 
-__author__ = "123456789"
+__author__ = "730474766"
+
+
+# unit tests for invert
+def test_invert_key_error() -> None:
+    with pytest.raises(KeyError):
+        """Testing for KeyError in invert with repeat keys."""
+        invert({'a': 'b', 'm': 'b'})
+
+
+def test_invert_empty() -> None:
+    """Testing invert for an empty dictionary."""
+    assert invert({}) == {}
+
+
+def test_invert_use() -> None:
+    """Testing invert for a simple use case."""
+    assert invert({'a': 'b', 'z': 'm', 'm': 'a'}) == ({'b': 'a', 'm': 'z', 'a': 'm'})
+
+
+def test_invert_second_use() -> None:
+    """Testing invert for another simple use case."""
+    assert invert({'sir': 'maam', 'boy': 'girl'}) == ({'maam': 'sir', 'girl': 'boy'})
+
+
+# unit tests for favorite_color
+def test_favorite_color_empty() -> None:
+    """Testing favorite_color for an empty dictionary."""
+    assert favorite_color({}) == ''
+
+
+def test_favorite_color_use() -> None:
+    """Testing favorite_color for a simple use case."""
+    assert favorite_color({'Mary': 'blue', 'Cindy': 'green'}) == 'blue'
+
+
+def test_favorite_color_second_use() -> None:
+    """Testing favorite_color for a second use case."""
+    assert favorite_color({'Terry': 'red', 'Maddy': 'blue', 'Cheryl': 'red', 'Amethyst': 'orange'}) == 'red'
+
+
+# unit tests for count
+def test_count_empty() -> None:
+    """Testing count for an empty list."""
+    assert count([]) == {}
+
+
+def test_count_use() -> None:
+    """Testing count for a simple use case."""
+    assert count(['perry', 'sam', 'george']) == {'perry': 1, 'sam': 1, 'george': 1}
+
+
+def test_count_second_use() -> None:
+    """Testing count for a second use case."""
+    assert count(['a', 'c', 'b', 'b', 'd', 'a', 'a']) == {'a': 3, 'c': 1, 'b': 2, 'd': 1}
